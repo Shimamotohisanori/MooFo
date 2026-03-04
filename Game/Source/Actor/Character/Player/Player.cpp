3f83d35132f065ddf8c017c5c0aebdb6006a8cbc
unknown <sennbatomohiro@gmail.com>
2026-03-04 11:19:50 +0900

    プレイヤーに移動および回転を追加

--- Game/Source/Actor/Character/Player/Player.cpp
+++ Game/Source/Actor/Character/Player/Player.cpp
@@ -23,6 +23,7 @@ Player::~Player()
 bool Player::Start()
 {
 	m_playerModelRender.Init(FILEPATH);
+
 	m_playerModelRender.SetPosition(m_transform.GetPosition());
 	m_characterController.Init(CHRACTER_CONTROLLER_WIDTH, CHRACTER_CONTROLLER_HIGHT, m_transform.GetPosition());
 	return true;
@@ -31,6 +32,8 @@ bool Player::Start()
 void Player::Update()
 {
 	Move();
+
+	Rotation();
 
 	m_playerModelRender.Update();
 }
@@ -50,26 +53,41 @@ void Player::Move()
 	Vector3 forward = g_camera3D->GetForward();
 	Vector3 right   = g_camera3D->GetRight();
 
-	//y方向には移動させない
-	forward.y = 0.0f;
+	//正規化
 	right.y = 0.0f;
-	//左スティックの入力量と150.0fを乗算
-	right *= stickL.x * 150.0f;
+	forward.y = 0.0f;
+	forward.Normalize();
+	right.Normalize();
+
+	//入力量を反映
+	Vector3 moveDir = forward * stickL.z* 150.0f + right * stickL.x * 150.0f;
+	m_moveSpeed.x = moveDir.x;
+	m_moveSpeed.z = moveDir.z;
+
+	//左スティックの入力量と120.0fを乗算
+	right*= stickL.x * 150.0f;
 	forward *= stickL.z * 150.0f;
 
 	//移動速度にカメラの前方向と右方向を加算
 	m_moveSpeed += right + forward;
-
-	//移動速度にスティックの入力量を掛ける
-	m_moveSpeed.x += stickL.x * 150.0f;
-	m_moveSpeed.z += stickL.z * 150.0f;
-
 	//キャラクターコントローラーを使って座標を移動させる
-	m_transform.GetPosition() = m_characterController.Execute(m_moveSpeed, 1.0f / 60.0f);
+	m_transform.GetPosition() = m_characterController.Execute(m_moveSpeed,g_gameTime->GetFrameDeltaTime());
 
 	//モデルの座標をキャラクターコントローラーの座標に合わせる
 	m_playerModelRender.SetPosition(m_transform.GetPosition());
 
+}
+
+void Player::Rotation()
+{
+	if (fabsf(m_moveSpeed.x) >= 0.0001f || fabsf(m_moveSpeed.z) >= 0.0001f)
+	{
+		//キャラクターの方向を変える
+		m_transform.GetRotation().SetRotationYFromDirectionXZ(m_moveSpeed);
+
+		//モデルの回転をキャラクターの回転に合わせる
+		m_playerModelRender.SetRotation(m_transform.GetRotation());
+	}
 }
 
 void Player::Render(RenderContext& rc)
