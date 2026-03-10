6eaf8cf59e260241923117df3b2bb45f15c5e63e
unknown <sennbatomohiro@gmail.com>
2026-03-10 11:10:20 +0900

    牛の移動及び回転する牛の追加

--- Game/Game.cpp
+++ Game/Game.cpp
@@ -10,8 +10,14 @@ bool Game::Start()
 {
 	//m_modelRender.Init("Assets/modelData/unityChan.tkm");
 	m_player = NewGO < Player>(0, "player");
+	//ステージの生成
 	m_stage = NewGO<Stage>(0, "stage");
+	//ランダムに移動する牛の生成
 	m_cow = NewGO<Cow>(0, "cow");
+	//回転ステートがスピンの牛の生成
+	Cow* spinCow = NewGO<Cow>(0, "spinCow");
+	spinCow->m_rotationState = Cow::EnRotatitonState_Spin;
+
 	m_gameCamera = NewGO<GameCamera>(0, "gameCamera");
 	m_UFO = NewGO<UFO>(0, "UFO");
 	return true;
