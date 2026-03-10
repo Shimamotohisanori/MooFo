12fe8b22ba6ff418955a3d7d97b213830b7437cd
SennbaTomohiro <sennbatomohiro@gmail.com>
2026-03-10 11:16:57 +0900

    牛の移動及び回転する牛の追加 (#6)

--- Game/Source/Actor/Character/Cow/Cow.cpp
+++ Game/Source/Actor/Character/Cow/Cow.cpp
@@ -1,12 +1,20 @@
-#include "stdafx.h"
+﻿#include "stdafx.h"
 #include "Cow.h"
+#include <time.h>
 namespace
 {
-	const char* FILEPATH = "Assets/modelData/Cow/Model/Cow.tkm"; //enModelUpAxis = enModelUpAxisZ;
+	const char* FILEPATH = "Assets/modelData/Cow/Model/Cow4.tkm"; //enModelUpAxis = enModelUpAxisZ;
+	const char* FILEPATH_IDLE = "Assets/modelData/Cow/Animation/Idle2.tka";
+	const char* FILEPATH_WALK = "Assets/modelData/Cow/Animation/Walk.tka";
 }
 Cow::Cow()
 {
-
+	//Idle
+	animationClips[EnAnimation_Idle].Load(FILEPATH_IDLE);
+	animationClips[EnAnimation_Idle].SetLoopFlag(true);
+	//Walk
+	animationClips[EnAnimation_Walk].Load(FILEPATH_WALK);
+	animationClips[EnAnimation_Walk].SetLoopFlag(true);
 }
 
 Cow::~Cow()
@@ -16,20 +24,126 @@ Cow::~Cow()
 
 bool Cow::Start()
 {
-	m_CowmodelRender.Init(FILEPATH);
+	srand(time(nullptr));
+
+	m_CowmodelRender.Init(FILEPATH,animationClips,EnAnimation_Num,enModelUpAxisZ);
 	return true;
 }
 
 void Cow::Update()
 {
+
+	/*アニメーション*/
+	PlayAnimation();
+
+  if (m_rotationState == EnRotationState_MoveDir)
+  {
+		/*移動*/
+		Move();
+  }
+	
+	/*ステート*/
+	ManageState();
+	/*回転*/
+	Rotation();
+	
+	/*モデルの更新*/
 	m_CowmodelRender.Update();
+
+	/*モデルの位置を反映*/
+	m_CowmodelRender.SetPosition(m_transform.GetPosition());
+	/*モデルに回転を反映*/
+	m_CowmodelRender.SetRotation(m_transform.GetRotation());
 }
 
 
 void Cow::Move()
 {
+	//タイマーが0以上なら新しい方向を決める
+	if (m_moveTimer <= 0)
+	{
+		Vector3 dir
+		(
+			//(0,1,2,から-1を引いているので)-1,0,1の範囲でランダムな値を生成
+			rand() % 3 - 1,
+			0,
+			rand() % 3 - 1
+		);
 
+		// 0,0,0になったら一秒休む
+		if (dir.LengthSq() == 0) 
+		{
+			m_moveDir = Vector3::Zero;
+			m_moveTimer = 60.0f;//1秒休む
+		}
+		else
+		{
+			dir.Normalize();
+			m_moveDir = dir;
+			m_moveTimer = 120.0f;//2秒ごとに方向を変える
+		}
+	}
+	//移動
+	Vector3 pos = m_transform.GetPosition();
+	//少しづつ位置を動かしている
+	pos += m_moveDir * m_moveSpeed * g_gameTime->GetFrameDeltaTime();
+	//ポジションを更新
+	m_transform.SetPosition(pos);
+
+	m_CowmodelRender.SetPosition(m_transform.GetPosition());
+
+	//タイマーを減らす
+	m_moveTimer--;
 }
+void Cow::Rotation()
+{
+	//移動ステートの時は移動方向に回転する。
+	if (m_rotationState == EnRotationState_MoveDir)
+	{
+		if (fabsf(m_moveDir.x) >= 0.0001f || fabsf(m_moveDir.z) >= 0.0001f)
+		{
+			//移動方向に回転させる
+			m_transform.GetRotation().SetRotationYFromDirectionXZ(m_moveDir);
+			m_transform.SetRotation(m_transform.GetRotation());
+		}
+	}
+	else if (m_rotationState == EnRotatitonState_Spin)
+	{
+		//回転ステートがスピンのときは常に回転する。
+		m_transform.GetRotation().AddRotationDegY(3.0f);
+		m_transform.SetRotation(m_transform.GetRotation());
+	}
+}
+
+void Cow::ManageState()
+{
+	if (fabsf(m_moveDir.x) >= 0.0001f || fabsf(m_moveDir.z) >= 0.0001f)
+	{
+		m_cowState = 1;//歩き
+	}
+	else
+	{
+		m_cowState = 0;//待機
+	}
+}
+
+void Cow::PlayAnimation()
+{
+	switch (m_cowState)
+	{
+	case 0 :
+			m_CowmodelRender.PlayAnimation(EnAnimation_Idle);
+			break;
+
+	case 1:
+			m_CowmodelRender.PlayAnimation(EnAnimation_Walk);
+			break;
+
+	default:
+		break;
+	}
+}
+
 
 void Cow::Render(RenderContext& rc)
 {
--- Game/Source/Actor/Character/Cow/Cow.h
+++ Game/Source/Actor/Character/Cow/Cow.h
@@ -10,9 +10,34 @@ public:
 		bool Start() override;
 		void Update() override;
 		void Render(RenderContext& rc) override;
-
+public:
 		void Move()override;
+		void Rotation();
+		void PlayAnimation();
+		void ManageState();
+public:
+	enum EnRotationState
+	{
+		EnRotationState_MoveDir,
+		EnRotatitonState_Spin,
+		EnRotationState_Num
+	};
+	EnRotationState m_rotationState = EnRotationState_MoveDir;//回転ステート
 private:
 	ModelRender m_CowmodelRender;
+	uint8_t m_cowState;//プレイヤーステート
+
+	Vector3 m_moveDir = Vector3::Zero;//移動方向
+	float m_moveSpeed = 50.0f;//移動速度
+	int m_moveTimer = 0;//移動タイマー
+	enum EnAnimation
+	{
+		EnAnimation_Idle,
+	    EnAnimation_Walk,
+		EnAnimation_Num
+		//EnAnimation_Run,
+		//EnAnimation_Jump,
+	};
+	AnimationClip animationClips[EnAnimation_Num];
 };
 
