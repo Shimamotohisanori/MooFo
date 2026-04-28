6c3808b3e3ade6f575fbb3fe8616e01e249bf15a
Shimamoto Hisanori <ca01254009@st.kawahara.ac.jp>
2026-04-28 13:07:46 +0900

    スコアの変動をカウンターアニメーション式に表示するように変更

--- Game/Score/Score.cpp
+++ Game/Score/Score.cpp
@@ -6,15 +6,20 @@ namespace
 	/** 三桁の数字を描画する画像の大きさ */
 	const Vector3 DIGIT_SCALE = { 0.0f,0.0f, 0.0f };
 
-	//数字の大きさ
+	/** 数字の大きさ */
 	const int  GAMECLEAR_WIDTH = 70.0f;
 	const int HEIGHT= 85.0f;
+
+	/** Lerp関数の補完率 */
+	constexpr float LERP_SPEED = 0.2f;
 }
+
 
 Score::Score()
 {
 
 }
+
 
 Score::~Score()
 {
@@ -25,31 +30,36 @@ Score::~Score()
 bool Score::Start()
 {
 	m_score = 0;
-	//if (m_score == m_prevScore){return;}
-	// このコードが値が同じなら早期リターンで画像が表示されなくなるから
-	//最初は絶対更新するためここを-1する
+
+	/** 最初は絶対更新するためここを - 1する */
 	m_prevScore = -1;
-	//全ての画像スプライト(50個)を初期化
+
+	/** 全ての画像スプライト(50個)を初期化 */
 	for (int j = 0; j < 10; j++)
 	{
 		for (int i = 0; i < 5; i++)
 		{
-			//画像の読み込みと大きさ調整
+			/** 画像の読み込みと大きさ調整 */
 			m_digitSprite[i][j].Init(m_digitPaths[j], GAMECLEAR_WIDTH,HEIGHT);
 
-			//非表示にしないと前のスコアが残ってしまいバグってしまう。
+			/** 非表示にしないと前のスコアが残ってしまいバグってしまう。 */
 			m_digitSprite[i][j].SetScale(DIGIT_SCALE);
 		}
 	}
 	return true;
 }
 
+
 void Score::Update()
 {
+	
+	/** スコアのカウンターアニメーション処理 */
+	ScoreCounterAnimation();
 
+	/** スコアの描画処理 */
 	TextScore();
 
-	//これがないと表示されない
+	/** スプライトの更新処理 */
 	for (int i = 0; i < 5; i++)
 	{
 		for (int j = 0; j < 10; j++)
@@ -62,16 +72,17 @@ void Score::Update()
 
 void Score::TextScore()
 {
-	//スコアが変わったときだけ更新する
-	if (m_score == m_prevScore)
+	/** スコアが変わったときだけ更新する */
+	if (m_displayScore == m_prevScore)
 	{
 		return;
 	}
-	//スコアを保存
-	m_prevScore = m_score;
-	int score = m_score;
 
-	//桁分解
+	/** スコアを保存 */
+	m_prevScore = m_displayScore;
+	int score = m_displayScore;
+
+	/** 桁分解 */
 	int digit[5];
 	for (int i = 0; i < 5; i++)
 	{
@@ -79,7 +90,7 @@ void Score::TextScore()
 		score /= 10;
 	}
 
-	//全部非表示
+	/** 全部非表示 */
 	for (int i = 0; i < 5; i++)
 	{
 		for (int j = 0; j < 10; j++)
@@ -89,20 +100,21 @@ void Score::TextScore()
 		}
 	}
 
-//表示(右から並べる)
+	/** 表示(右から並べる) */
 	float baseX;
 	float baseY;
 
 	if (m_isResult)
 	{
 		baseY = -100.0f;
-		//ゲームクリアならスコアを-580の位置に表示する
+		/** ゲームクリアならスコアを表示する */
 		if (m_resultType == ResultType::GameClear)
 		{
 			baseX = -700.0f;
 			baseY = -100.0f;
 		}
-		//ゲームオーバーならスコアを-680の位置に表示する
+
+		/** ゲームオーバーならスコアを表示する */
 		else if (m_resultType == ResultType::GameOver)
 		{
 			baseX = -700.0f;
@@ -111,7 +123,7 @@ void Score::TextScore()
 	}
 	else
 	{
-		//ゲーム中なら
+		/** ゲーム中なら */
 		baseX = -900.0f;
 		baseY = -450.0f;
 	}
@@ -121,25 +133,31 @@ void Score::TextScore()
 		int d = digit[i];
 
 		int index = 4 - i;
-		//右→左に変換
+
+		/** 右→左に変換 */
 		m_digitSprite[index][d].SetScale({1,1,1});
 
-		//位置調整(横並び)
+		/** 位置調整(横並び) */
 		m_digitSprite[index][d].SetPosition({
-			baseX + index * 80,//
+			baseX + index * 80,
 			baseY,//下側
 			0.0f });
 	}
 	
 }
+
+
 void Score::AddScore(int value)
 {
 	m_score += value;
 }
+
+
 void Score::DecreaseScore(int value)
 {
 	m_score -= value;
-	//スコアがマイナスにならないようにする
+
+	/** スコアがマイナスにならないようにする */
 	if (m_score < 0)
 	{
 		m_score = 0;
@@ -159,11 +177,26 @@ void Score::SetScore(int score)
 }
 
 
+void Score::ScoreCounterAnimation()
+{
+	/** Lerp関数で滑らかに値を近づける */
+	/** 表示するスコアを徐々にスコアに近づける */
+	/** 型変換をすることで、整数の計算を行う */
+	m_displayScore = m_displayScore + (int)((m_score - m_displayScore) * LERP_SPEED);
+
+	/** 絶対値が5未満ならスコアを到達させる */
+	if (abs(m_score - m_displayScore) < 5)
+	{
+		m_displayScore = m_score;
+	}
+}
+
 
 int Score::GetScore() const
 {
 	return m_score;
 }
+
 
 void Score::Render(RenderContext& rc)
 {
