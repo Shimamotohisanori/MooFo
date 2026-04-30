b090ca90741a8f63f95324623a62fc0ac2de52a0
Shimamoto Hisanori <ca01254009@st.kawahara.ac.jp>
2026-04-30 14:02:50 +0900

    タイトル画面でのフェード処理及びボタンを押した際の点滅処理を追加

--- Game/GameScene/Title.cpp
+++ Game/GameScene/Title.cpp
@@ -6,9 +6,19 @@
 
 namespace
 {
-	const char* GAMECLEAR_FILEPATH = "Assets/sprite/GameTransition/GameTitle.dds";
-	const int GAMECLEAR_WIDTH = 1920;
-	const int GAMEOVER_HIGHT = 1080;
+	const char* GAMETITLE_FILEPATH = "Assets/sprite/GameTransition/GameTitle.dds";
+	const char* PRESSSTART_FILEPATH = "Assets/sprite/GameTransition/PressAnyButton.dds";
+
+	const Vector3 PRESSSTART_POS = { 0.0f, -350.0f, 0.0f };
+
+	const int GAMETITLE_WIDTH = 1920;
+	const int GAMETITLE_HEIGHT = 1080;
+
+	constexpr int PRESSSTART_WIDTH = 1400;
+	constexpr int PRESSSTART_HEIGHT = 1700;
+
+	/** タイトル画面での点滅の最後の間隔 */
+	constexpr float TITLE_FINAL_BLINK_INTERVAL = 1.0f;
 }
 Title::Title()
 {
@@ -23,10 +33,14 @@ Title::~Title()
 
 bool Title::Start()
 {
-	m_TitleSpriteRender.Init(GAMECLEAR_FILEPATH,GAMECLEAR_WIDTH,GAMEOVER_HIGHT);
+	m_titleSpriteRender.Init(GAMETITLE_FILEPATH, GAMETITLE_WIDTH, GAMETITLE_HEIGHT);
+
+	m_pressStartSpriteRender.Init(PRESSSTART_FILEPATH, PRESSSTART_WIDTH, PRESSSTART_HEIGHT);
+	m_pressStartSpriteRender.SetPosition(PRESSSTART_POS);
+	m_pressStartSpriteRender.Update();
 
 	m_soundManager = FindGO<SoundManager>("soundmanager");
-	p_titleBGM = m_soundManager->PlayingBGM(SoundBGM::enTitleBGM, false);
+	m_titleBGM = m_soundManager->PlayingBGM(SoundBGM::enTitleBGM, false);
 
 	return true;
 }
@@ -36,28 +50,110 @@ void Title::Update()
 {
 	InTitle();
 
-	m_TitleSpriteRender.Update();
+	FadeTitle();
+
+	m_titleSpriteRender.Update();
+	m_pressStartSpriteRender.Update();
 }
 
 
 void Title::InTitle()
 {
-	if (g_pad[0]->IsPressAnyKey())
+	if (g_pad[0]->IsPressAnyKey() && !m_isButtonPressed)
 	{
-		m_loadingScene = NewGO<LoadingScene>(0, "loading");
-		m_loadingScene->SetLoadType(LoadingScene::LoadType::ToGameScene);
-		p_titleSE = m_soundManager->PlayingSE(SoundSE::enDecisionSE, false);
-		DeleteGO(p_titleBGM);
-		m_loadingScene->SetNextScene([]()
+		/* ボタンを押したときの処理 */
+		m_isButtonPressed = true;
+
+		m_titleSE = m_soundManager->PlayingSE(SoundSE::enDecisionSE, false);
+
+	}
+
+	if (m_isButtonPressed)
+	{
+		/* 点滅の間隔を減らしていく */
+		m_blinkInterval -= g_gameTime->GetFrameDeltaTime() * m_blinkInterval;
+
+		/* 点滅の間隔が最後の間隔以下になったときに、タイトルからローディングシーンに移行する */
+		if (m_blinkInterval <= TITLE_FINAL_BLINK_INTERVAL)
+		{
+			m_isButtonPressed = false;
+			m_loadingScene = NewGO<LoadingScene>(0, "loading");
+			m_loadingScene->SetLoadType(LoadingScene::LoadType::ToGameScene);
+			DeleteGO(m_titleBGM);
+			m_loadingScene->SetNextScene([]()
+				{
+					NewGO<Game>(0, "game");
+				});
+			DeleteGO(this);
+		}
+	}	
+}
+
+void Title::FadeTitle()
+{
+	if (!m_isButtonPressed)
+	{
+		/** フェード処理*/
+		switch (m_titleState)
+		{
+		case FadeIn:
+			m_alpha += g_gameTime->GetFrameDeltaTime();
+			if (m_alpha >= 1.0f)
 			{
-				NewGO<Game>(0, "game");
-			});
-		DeleteGO(this);
+				m_alpha = 1.0f;
+				m_titleState = FadeOut;
+			}
+			break;
+
+		case FadeOut:
+			m_alpha -= g_gameTime->GetFrameDeltaTime();
+			if (m_alpha <= 0.0f)
+			{
+				m_alpha = 0.0f;
+				m_titleState = FadeIn;
+			}
+			break;
+		}
 	}
 }
 
 void Title::Render(RenderContext& rc)
 {
-	m_TitleSpriteRender.Draw(rc);
+	m_titleSpriteRender.Draw(rc);
+
+	/* ボタンが押されていないときはスタートを促すスプライトをフェード描画する */
+	if (!m_isButtonPressed)
+	{
+		/** α値が0.0より大きいときに描画する */
+		if (m_alpha > 0.0f)
+		{
+			m_pressStartSpriteRender.SetMulColor(
+				Vector4(
+					1.0f,
+					1.0f,
+					1.0f,
+					m_alpha));
+
+			m_pressStartSpriteRender.Draw(rc);
+		}
+
+		return;
+	}
+
+	/** 点滅処理 */
+	/* 点滅の間隔が偶数のときは描画する */
+	if(static_cast<int>(m_blinkInterval * 2) % 2 == 0)
+	{
+		m_pressStartSpriteRender.SetMulColor(
+			Vector4(
+				1.0f,
+				1.0f,
+				1.0f,
+				1.0f));
+		m_pressStartSpriteRender.Draw(rc);
+	}
+
 }
+
+
 
--- Game/GameScene/Title.h
+++ Game/GameScene/Title.h
@@ -11,13 +11,38 @@ public:
 	void InTitle();
 	void Render(RenderContext& rc);
 
+
 private:
-	SpriteRender m_TitleSpriteRender;//タイトルのスプライトレンダー
+	/** フェード用関数 */
+	void FadeTitle();
+
+private:
+	SpriteRender m_titleSpriteRender;//タイトルのスプライトレンダー
+
+	/** スタートを促すスプライトレンダー */
+	SpriteRender m_pressStartSpriteRender;
 
 	SoundManager* m_soundManager;
-	SoundSource* p_titleBGM;
-	SoundSource* p_titleSE;
+	SoundSource* m_titleBGM;
+	SoundSource* m_titleSE;
 	LoadingScene* m_loadingScene;
 
+	enum enTitleState
+	{
+		/** タイトルのフェードイン */
+		FadeIn,
+		/** タイトルのフェードアウト */
+		FadeOut,
+	};
+	enTitleState m_titleState = FadeIn;
+
+	/** α値の変数 */
+	float m_alpha = 0.0f;
+
+	/** 点滅の間隔 */
+	float m_blinkInterval = 50.0f;
+
+	/** ボタンを押したときのフラグ */
+	bool m_isButtonPressed = false;
 };
 
