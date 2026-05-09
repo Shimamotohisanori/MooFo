7502cbccd750525e3785acb94365033d4fd22006
unknown <sennbatomohiro@gmail.com>
2026-05-09 15:52:27 +0900

    UFOの光をスポットライトに変更

--- Game/Source/Actor/Character/UFO/CowCaptureController.h
+++ Game/Source/Actor/Character/UFO/CowCaptureController.h
@@ -55,18 +55,29 @@ public:
 	/** 秒数の描画処理*/
 	void CountSpriteUI();
 private:
+	/** スポットライト*/
+	nsK2Engine::SpotLight m_ufoSpotLight;
+	/** モデルレンダー*/
 	ModelRender m_ufocontrollermodelRender;
-	SpriteRender m_LightApperNumberSpriteRender[5];/** 光が出ているときに表示する数字のスプライトレンダー*/
-	SpriteRender m_LightApperSpriteRender;/** 「光の発射まで」を表示するスプライトレンダー*/
-	SpriteRender m_secondsSpriteRender;/** 「秒」を表示するスプライトレンダー*/
-	int m_currentCount = 0;/** 現在の秒数*/
+	/** 光が出ているときに表示する数字のスプライトレンダー*/
+	SpriteRender m_LightApperNumberSpriteRender[5];
+	/** 「光の発射まで」を表示するスプライトレンダー*/
+	SpriteRender m_LightApperSpriteRender;
+	/** 「秒」を表示するスプライトレンダー*/
+	SpriteRender m_secondsSpriteRender;
+	/** 現在の秒数*/
+	int m_currentCount = 0;
+	/**光が出るまでの時間*/
+	float m_waitTimer = 5.0f;
+	/** 光が出ている時間*/
+	float m_emitTimer = 5.0f;
+	/**タイマー*/
+	float m_timer = 0.0f;
+	/** 光が出ているかどうかのフラグ*/
+	bool m_isEmitting = false;
+	/** 牛を捕獲しているかどうかのフラグ*/
+	bool m_isCapturing = false;
 
-	float m_waitTimer = 5.0f;/**光が出るまでの時間*/
-	float m_emitTimer = 5.0f;/** 光が出ている時間*/
-	float m_timer = 0.0f;/**タイマー*/
-	//FontRender m_countText;/** 秒数の描画用のフォントレンダラー*/
-	bool m_isEmitting = false;/** 光が出ているかどうかのフラグ*/
-	bool m_isCapturing = false;/** 牛を捕獲しているかどうかのフラグ*/
 	UFO* m_ufo = nullptr;
 	Pause* m_pause;
 	CountDown* m_countdown;
