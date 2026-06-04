4213015ce138f6abc6eec3651a25845867e92221
KaechiKensuke <ca01254005@st.kawahara.ac.jp>
2026-06-04 14:19:42 +0900

    牛の餌の上にエフェクトを再生させる

--- Game/Source/Actor/Stage/CowFood.h
+++ Game/Source/Actor/Stage/CowFood.h
@@ -11,7 +11,15 @@ public:
 	void Render(RenderContext& rc);
 
 private:
+
+	bool m_iseffect = false; 
+
+	/** 牛の餌のモデル */
 	ModelRender m_cowFoodModelRender;
 
+	/** 牛の餌の当たり判定 */
 	PhysicsStaticObject m_FoodObject;
+
+	/** 牛の餌のエフェクト */
+	EffectEmitter* m_cowFoodEffect;
 };
