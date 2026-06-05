a43a724655959a6d25e919028c4432809201f249
KaechiKensuke <ca01254005@st.kawahara.ac.jp>
2026-06-05 14:32:46 +0900

    牛の餌のUIを表示させる

--- Game/Source/Actor/Stage/CowFood.cpp
+++ Game/Source/Actor/Stage/CowFood.cpp
@@ -7,12 +7,21 @@ namespace
 	/** 牛の餌のモデルファイルパス */
 	const char* COWFOOD_FILE_PATH = "Assets/modelData/Stage/CowFood.tkm";
 
-	const char* COWFOOD_UI = "Assets/sprite/CowFoodUI/test.dds";
+	const char* BUTTON_UI = "Assets/sprite/CowFoodUI/test.dds";
+
+	const char* FOODBUCKET_UI = "Assets/sprite/CowFoodUI/FoodBucket.dds";
+
+	const char* FOODBUCKET_BLACK_UI = "Assets/sprite/CowFoodUI/FoodBucketBlack.dds";
 
 	/** 牛の餌の座標 */
-	Vector3 COWFOOD_POS = { -1260.0f,-5.0f,-350.0f };
+	const Vector3 COWFOOD_POS = { -1260.0f,-5.0f,-350.0f };
 
-	Vector3 COWFOOD_SCALE = { 40.0f,40.0f,40.0f };
+	/** 牛の餌のUIの座標 */
+	const Vector3 FOODUI_POS       = { -850.0f,-250.0f,0.0f };
+	const Vector3 FOODUI_SECOND_POS = { -700.0f,-250.0f,0.0f };
+
+	/** 牛の餌のエフェクトの大きさ */
+	const Vector3 COWFOOD_EFFECT_SCALE = { 40.0f,40.0f,40.0f };
 
 }
 
@@ -23,19 +32,45 @@ bool CowFood::Start()
 	m_cowFoodModelRender.SetPosition(COWFOOD_POS);
 	m_cowFoodModelRender.Update();
 
-	m_Abutton.Init(COWFOOD_UI,1280.0f,1080.0f);
+	/** AボタンのUI */
+	m_Abutton.Init(BUTTON_UI,280.0f,108.0f);
+
+	/** 牛の餌のUI(左) */
+	m_bucketFood.Init(FOODBUCKET_UI, 100.0f, 100.0f);
+	m_bucketFood.SetPosition(FOODUI_POS);
+	m_bucketFood.Update();
+
+	/** 牛の餌のUI(右) */
+	m_bucketFood_Second.Init(FOODBUCKET_UI, 100.0f, 100.0f);
+	m_bucketFood_Second.SetPosition(FOODUI_SECOND_POS);
+	m_bucketFood_Second.Update();
+
+	/** 牛の餌のシルエットUI(左) */
+	m_bucketFood_Black.Init(FOODBUCKET_BLACK_UI, 100.0f, 100.0f);
+	m_bucketFood_Black.SetPosition(FOODUI_POS);
+	m_bucketFood_Black.Update();
+
+	/** 牛の餌のシルエットUI(右) */
+	m_bucketFood_SecondBlack.Init(FOODBUCKET_BLACK_UI, 100.0f, 100.0f);
+	m_bucketFood_SecondBlack.SetPosition(FOODUI_SECOND_POS);
+	m_bucketFood_SecondBlack.Update();
+
+	/** シルエットとかまでも作れたからあとは描画処理作って
+	カウントを利用してね*/
 
 	/** 牛の餌の当たり判定をつける */
 	m_FoodObject.CreateFromModel(m_cowFoodModelRender.GetModel(), m_cowFoodModelRender.GetModel().GetWorldMatrix());
 
+	/** 餌のエフェクトを出す */
 	m_cowFoodEffect = NewGO<nsK2EngineLow::EffectEmitter>(0);
 
 	m_cowFoodEffect->Init((int)EffectID::EffectID_CowFoodEffect);
 
+	/** 牛の餌のエフェクトのポジション */
 	Vector3 cowFoodPos = { COWFOOD_POS.x, COWFOOD_POS.y + 30.0f, COWFOOD_POS.z + 10.0f };
 
 	m_cowFoodEffect->SetPosition(cowFoodPos);
-	m_cowFoodEffect->SetScale(COWFOOD_SCALE);
+	m_cowFoodEffect->SetScale(COWFOOD_EFFECT_SCALE);
 	
 
 	return true;
@@ -43,10 +78,13 @@ bool CowFood::Start()
 
 void CowFood::Update()
 {
+	/** プレイヤーのクラスを見つける */
 	Player* player = FindGO<Player>("player");
 
+	/** もしエフェクト再生中じゃないなら */
 	if (!m_cowFoodEffect->IsPlay())
 	{
+		/** エフェクトを再生させる */
 		m_cowFoodEffect->Play();
 	}
 
@@ -55,22 +93,27 @@ void CowFood::Update()
 		return;
 	}
 
+	/** 牛の餌のポジションを変数に代入させる */
 	m_position = COWFOOD_POS;
 	m_cowFoodModelRender.SetPosition(m_position);
 
+	/** プレイヤーの距離から牛の餌の距離を計算する */
 	Vector3 dir = player->GetPosition() - m_position;
 	dir.y = 0.0f;
 
+	/** 二つの距離のベクトルの長さ */
 	float distance = dir.Length();
 
+	/** もし、100.0f以内なら */
 	if (distance <= 100.0f)
 	{
-		m_iseffect = true;
+		/** ボタンのUIを表示させる。 */
+		m_isButtonUI = true;
 		m_Abutton.Update();
 	}
 	else
 	{
-		m_iseffect = false;
+		m_isButtonUI = false;
 	}
 	
 }
@@ -80,9 +123,13 @@ void CowFood::Render(RenderContext& rc)
 	/** 牛の餌のモデルを描画する。 */
 	m_cowFoodModelRender.Draw(rc);
 
-	if (m_iseffect)
+	/** フラグがtrueならボタンのUIを描画させる。*/
+	if (m_isButtonUI)
 	{
 		m_Abutton.Draw(rc);
 	}
+
+	m_bucketFood.Draw(rc);
+	m_bucketFood_Second.Draw(rc);
 	
 }
--- Game/Source/Actor/Stage/CowFood.h
+++ Game/Source/Actor/Stage/CowFood.h
@@ -10,16 +10,40 @@ public:
 	void Update();
 	void Render(RenderContext& rc);
 
+	uint8_t GetFoodCount()
+	{
+		return m_foodCount;
+	}
+
+	void SetFoodCount(uint8_t foodCount)
+	{
+		m_foodCount = foodCount;
+	}
+
 private:
 
-	bool m_iseffect = false; 
+	/** 牛の餌を所持している数 */
+	uint8_t m_foodCount = 0;
 
+	/** UIを表示させるフラグ */
+	bool m_isButtonUI = false; 
+
+	/** 牛の餌のポジション */
 	Vector3 m_position;
 
 	/** 牛の餌のモデル */
 	ModelRender m_cowFoodModelRender;
 
+	/** Aボタンを描画させる画像 */
 	SpriteRender m_Abutton;
+
+	/** 餌を描画させる画像 */
+	SpriteRender m_bucketFood;
+	SpriteRender m_bucketFood_Second;
+
+	/** 餌のシルエットを描画させる画像 */
+	SpriteRender m_bucketFood_Black;
+	SpriteRender m_bucketFood_SecondBlack;
 
 	/** 牛の餌の当たり判定 */
 	PhysicsStaticObject m_FoodObject;
