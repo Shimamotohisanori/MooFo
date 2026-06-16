1c30930fd7ece6c6be305e8ebfa4d80d99e37329
KaechiKensuke <ca01254005@st.kawahara.ac.jp>
2026-06-16 14:35:33 +0900

    牛の餌のモデルの大きさの変更、AボタンのUIをAボタンのエフェクトに変更

--- Game/EffectManager/EffectManager.h
+++ Game/EffectManager/EffectManager.h
@@ -6,6 +6,7 @@ enum class EffectID
 	EffectID_UFOLight,/** UFOの光*/
 	EffectID_HeartEffect,/** 救出された牛が出すハート*/
 	EffectID_CowFoodEffect,/** 牛の餌*/
+	EffectID_Abutton, /** 牛の餌に近づくと表示されるAボタン */
 	EffectID_UFOConfusionEffect_1,/**救出されたら出るUFOの混乱エフェクト(1段階目)*/
 	EffectID_UFOConfusionEffect_2,/**救出されたら出るUFOの混乱エフェクト(2段階目)*/
 	EffectID_UFOConfusionEffect_3,/**救出されたら出るUFOの混乱エフェクト(3段階目)*/
